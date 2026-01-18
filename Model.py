00549f95a783ac04af340e1d96eba6edf03c300a
Awerdos <jakub.piskorowski1tlga@gmail.com>
2026-01-18 11:38:35 +0100

    Visualization

--- Model.py
+++ Model.py
@@ -2,19 +2,20 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt 
 
-def genes_population(sizeofgenom,n):
+def GenesPopulation(sizeofgenom,n):
     return [[1 if random.random()>0.5 else 0 for _ in range(sizeofgenom)] for _ in range(n)]
 
-def coding(c):
+def Coding(c):
     r = len(c) 
     sum = [c[i+2] * 2**(r-(i+3)) for i in range(len(c)-2)]
     return float((-1)**c[0] * np.exp((-1)**c[1]) * np.sum(sum))
 
-def fitness(x):
-    x = coding(x)
+def Fitness(x):
+    x = Coding(x)
     return abs(x**3 - x**2 + 1)
     # return x**2
     # return -1*(x**2) + 2*x +10 
+    # return -x**2+10*x+1
 
 def EliteSelection(fitness_genes,eliteSize,Minimize = False):
     
@@ -62,22 +63,22 @@ mean_list = []
 y_list = []
 
 # Tworzymy populacje 
-population = genes_population(7,100)
+population = GenesPopulation(7,100)
 
-for _ in range(5):
+for _ in range(1000):
     
     while True:
         
         # Ocena populacji 
-        fitness_genes = list(map(fitness,population))
+        fitness_genes = list(map(Fitness,population))
 
         # Selekcja 
-        elite_genes, rest_of_genes, sorted_fitness_genes = EliteSelection(fitness_genes,30)
+        elite_genes, rest_of_genes, sorted_fitness_genes = EliteSelection(fitness_genes,30,Minimize=False)
 
         # Warunek stopu
         elite_fitness_gens.append(np.mean(sorted_fitness_genes))
         if n > 0:
-            if abs(elite_fitness_gens[n] - elite_fitness_gens[n-1]) < 0.1:
+            if abs(elite_fitness_gens[n] - elite_fitness_gens[n-1]) < 10:
                 break
         
         # Krzyżowanie
@@ -89,31 +90,32 @@ for _ in range(5):
 
         n += 1
     
-    mapedvalues = list(map(fitness,population))
+    mapedvalues = list(map(Fitness,population))
     zipedelites = list(zip(population,mapedvalues))
     ranking = sorted(zipedelites, key=lambda x: x[1],reverse=False)
     bin, y = zip(*ranking)
     n_list.append(n)
-    x_list.append(coding(bin[0]))
+    x_list.append(Coding(bin[0]))
     y_list.append(y[0])
     mean_list.append(elite_fitness_gens)
 
-    population = genes_population(7,100)
+    population = GenesPopulation(7,100)
     elite_fitness_gens = []
     n = 0
 
-print(x_list)
+
 print(np.mean(x_list))
-# print(f"Największa wartość: {max(bin_list)}")
-# print(f"Najmniejsza wartość: {min(bin_list)}")
+print(f"Największa wartość: {max(x_list)}")
+print(f"Najmniejsza wartość: {min(x_list)}")
 print(np.mean(n_list))
 
 x = np.linspace(-2, 2, 100)
 y = abs(x**3 - x**2 + 1)
 
-fig = plt.figure(figsize = (10, 5))
-plt.axvline(x = 0, color = 'black')
-plt.axhline(y = 0 , color='black')
-plt.plot(x, y)
-plt.scatter(x_list,y_list, color = 'red', s=50, alpha=.5)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+ax2.hist(x_list,color = 'red')
+ax1.axvline(x = 0, color = 'black')
+ax1.axhline(y = 0 , color='black')
+ax1.plot(x, y)
+ax1.scatter(x_list,y_list, color = 'red', s=50, alpha=.3)
 plt.show()
